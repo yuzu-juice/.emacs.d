f26782d976d7de1ee0d61082a4f24df755d6e4ec
yuzu-juice <yuzujuicesec@gmail.com>
2025-11-05 11:44:19 +0900

    update: init.el

--- init.el
+++ init.el
@@ -1,3 +1,12 @@
+;;; init.el --- setting file -*- lexical-binding: t -*-
+
+;;; Author: yuzu-juice
+;;; Commentary:
+
+;;; Code:
+;; byte compile warning 非表示
+(setq byte-compile-warnings nil)
+
 ;; <leaf-install-code>
 (eval-and-compile
   (customize-set-variable
@@ -22,30 +31,55 @@
     (leaf-keywords-init)))
 ;; </leaf-install-code>
 
-;; leaf package install
-;;; magit
-(leaf magit :ensure t :bind (("C-x g" . magit-status)))
-;;; undo-tree
+;; leaf
+(leaf magit
+  :ensure t
+  :bind (("C-x g" . magit-status)))
 (leaf undo-tree
   :ensure t
   :global-minor-mode global-undo-tree-mode
   :custom
-  (undo-tree-auto-save-history . nil)
+  (undo-tree-auto-save-history . nil))
+(leaf gcmh
+  :ensure t
+  :global-minor-mode t
+  :custom
+  (gcmh-verbose . t))
+(leaf doom-themes
+  :ensure t
+  :config
+  (load-theme 'doom-solarized-light t))
+(leaf doom-modeline
+  :ensure t
+  :global-minor-mode t
 )
-;;; corfu
+(leaf beacon
+  :ensure t
+  :global-minor-mode t
+  :custom ((beacon-color . "#FAB27B")))
 (leaf corfu
   :ensure t
   :global-minor-mode global-corfu-mode
   :custom
   (corfu-cycle . t)
   (corfu-auto . t)
-  (text-mode-ispell-word-completion . nil)
-)
-
-;;; cape
+  (text-mode-ispell-word-completion . nil))
 (leaf cape
+  :ensure t)
+(leaf flymake
+  :global-minor-mode t)
+(leaf mozc
   :ensure t
-)
+  :config
+  (setq default-input-method "japanese-mozc"))
+(leaf :font
+  :config
+  (leaf nerd-icons
+    :ensure t)
+  )
+(leaf puni
+  :ensure t
+  :global-minor-mode puni-global-mode)
 
 ;; 現在位置表示
 (column-number-mode t)
@@ -53,12 +87,31 @@
 ;; 行番号表示
 (global-display-line-numbers-mode t)
 
+;; カーソルの色を変更
+(set-cursor-color "#F79428")
+
 ;; 最初の挨拶非表示
 (setq inhibit-startup-message t)
 
 ;; key bind
 (global-set-key (kbd "C-t") 'delete-window)
+(global-set-key (kbd "C-h") 'delete-backward-char)
 
-;; byte compile warning 非表示
-(setq byte-compile-warnings nil)
+;; UTF-8を優先
+(prefer-coding-system 'utf-8)
 
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;;; init.el ends here

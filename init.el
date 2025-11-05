98c6cca01c364369896f06667b11ae64c2b9ae71
yuzu-juice <yuzujuicesec@gmail.com>
2025-11-05 18:23:21 +0900

    update: init.el

--- init.el
+++ init.el
@@ -31,128 +31,139 @@
     (leaf-keywords-init)))
 ;; </leaf-install-code>
 
-;; leaf
-(leaf magit
+;;
+;; Core Emacs Settings
+;;
+(leaf emacs
+  :doc "Emacs-lisp standard library"
+  :tag "built-in"
+  :custom
+  ;; 現在位置表示
+  (column-number-mode . t)
+  ;; 行番号表示
+  (global-display-line-numbers-mode . t)
+  ;; 最初の挨拶非表示
+  (inhibit-startup-message . t)
+  ;; 最終行に空行を強制
+  (require-final-newline . t)
+  ;; 起動時に最後に開いていたファイルを開く
+  (desktop-save-mode . 1)
+  :config
+  ;; カーソルの色を変更
+  (set-cursor-color "#F79428")
+  ;; UTF-8を優先
+  (prefer-coding-system 'utf-8)
+
+  :bind (("C-t" . delete-window)
+         ("C-h" . delete-backward-char)))
+
+;;
+;; Ui Packages
+;;
+(leaf doom-themes
   :ensure t
-  :bind (("C-x g" . magit-status)))
+  :config
+  (load-theme 'doom-solarized-light t))
+
+(leaf doom-modeline
+  :ensure t
+  :global-minor-mode t)
+
+(leaf beacon
+  :ensure t
+  :global-minor-mode t
+  :custom ((beacon-color . "#FAB27B")))
+
+;;
+;; Editing Packages
+;;
 (leaf undo-tree
   :ensure t
   :global-minor-mode global-undo-tree-mode
   :custom
   (undo-tree-auto-save-history . nil))
+
 (leaf gcmh
   :ensure t
   :global-minor-mode t
   :custom
   (gcmh-verbose . t))
-(leaf doom-themes
-  :ensure t
-  :config
-  (load-theme 'doom-solarized-light t))
-(leaf doom-modeline
-  :ensure t
-  :global-minor-mode t
-)
-(leaf beacon
-  :ensure t
-  :global-minor-mode t
-  :custom ((beacon-color . "#FAB27B")))
-(leaf mozc
-  :ensure t
-  :config
-  (setq default-input-method "japanese-mozc"))
+
 (leaf puni
   :ensure t
   :global-minor-mode puni-global-mode)
+
+;;
+;; Programming Packages
+;;
 (leaf lsp-mode
   :ensure t
   :hook ((c-mode . lsp)
-	 (c++-mode . lsp))
+         (c++-mode . lsp))
   :commands lsp)
+
 (leaf lsp-ui
   :ensure t
   :after lsp-mode
   :commands lsp-ui-mode)
+
 (leaf company
   :ensure t
   :global-minor-mode global-company-mode)
+
 (leaf flycheck
   :ensure t
   :global-minor-mode global-flycheck-mode)
+
 (leaf projectile
   :ensure t
   :global-minor-mode projectile-mode)
-(leaf wakatime-mode
+
+(leaf diff-hl
   :ensure t
-  :custom
-  ((wakatime-cli-path . "/opt/homebrew/bin/wakatime-cli"))
+  :global-minor-mode t
+  :after magit
   :config
-  (global-wakatime-mode))
+  (add-hook 'magit-pre-refresh-hook #'diff-hl-magit-pre-refresh)
+  (add-hook 'magit-post-refresh-hook #'diff-hl-magit-post-refresh)
+  (add-hook 'magit-post-commit-hook #'diff-hl-update))
+
+;;
+;; Tools Packages
+;;
+(leaf magit
+  :ensure t
+  :bind (("C-x g" . magit-status)))
+
 (leaf treemacs
   :ensure t
   :bind
   ("<f8>" . treemacs)
   (:treemacs-mode-map
-   ([mouse-1] . #'treemacs-single-click-expand-action)
-   )
+   ([mouse-1] . #'treemacs-single-click-expand-action))
   )
+
 (leaf treemacs-magit
   :ensure t
-  :after (treemacs magit)
-  )
+  :after (treemacs magit))
+
 (leaf treemacs-projectile
   :ensure t
   :after (treemacs projectile)
-  )
-(leaf diff-hl
+  :config
+  (add-hook 'projectile-after-switch-project-hook
+            #'treemacs-display-current-project-exclusively))
+
+(leaf mozc
   :ensure t
   :config
-  (global-diff-hl-mode 1)
-  (with-eval-after-load 'magit
-    (add-hook 'magit-pre-refresh-hook #'diff-hl-magit-pre-refresh)
-    (add-hook 'magit-post-refresh-hook #'diff-hl-magit-post-refresh)
-    (add-hook 'magit-post-commit-hook #'diff-hl-update)))
+  (setq default-input-method "japanese-mozc"))
 
-;; 現在位置表示
-(column-number-mode t)
+(leaf wakatime-mode
+  :ensure t
+  :global-minor-mode t
+  :custom
+  ((wakatime-cli-path . "/opt/homebrew/bin/wakatime-cli")))
 
-;; 行番号表示
-(global-display-line-numbers-mode t)
-
-;; カーソルの色を変更
-(set-cursor-color "#F79428")
-
-;; 最初の挨拶非表示
-(setq inhibit-startup-message t)
-
-;; 最終行に空行を強制
-(setq require-final-newline t)
-
-;; 起動時に最後に開いていたファイルを開く
-(desktop-save-mode 1)
-
-;; key bind
-(global-set-key (kbd "C-t") 'delete-window)
-(global-set-key (kbd "C-h") 'delete-backward-char)
-
-;; UTF-8を優先
-(prefer-coding-system 'utf-8)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(all-the-icons beacon blackout cape company corfu doom-modeline
-		   doom-themes el-get flycheck gcmh hydra
-		   leaf-keywords lsp-ui magit mozc projectile puni
-		   shackle undo-tree volatile-highlights wakatime-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 ;;; init.el ends here

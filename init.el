0b33fa8ac5fc782116063f8cc4d89ff3a1ff2f87
yuzu-juice <yuzujuicesec@gmail.com>
2025-11-20 22:14:06 +0900

    test

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
 ;; byte compile warning 非表示
 (setq byte-compile-warnings nil)
 
-;; <leaf-install-code> test
+;; <leaf-install-code>
 (eval-and-compile
   (customize-set-variable
    'package-archives '(("org" . "https://orgmode.org/elpa/")
